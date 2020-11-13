5a922822324ffcb19fb72bfd65e482ec941e130a
Martyna Rezai <martyna.rezai@gmail.com>
2020-11-13 11:11:06 +0100

    add -add class 'active'

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+    
   
     /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
   
     /* add class 'active' to the clicked link */
+    console.log('clickedElement:', clickedElement){
+        activeLink.classList.add('active');
+    }
   
     /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('article.active');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
   
     /* get 'href' attribute from the clicked link */
   
