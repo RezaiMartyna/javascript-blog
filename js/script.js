9da3dcbf912abd6ccd2ab837c31af7b28e3380ff
Martyna Rezai <martyna.rezai@gmail.com>
2020-11-24 09:32:03 +0100

    start function genratetitlelinks

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 {
-  const titleClickHandler = function(event){
+  const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
@@ -8,7 +8,7 @@
     /* remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks){
+    for (let activeLink of activeLinks) {
       activeLink.classList.remove('active');
     }
 
@@ -20,7 +20,7 @@
     /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article.active');
 
-    for(let activeArticle of activeArticles){
+    for (let activeArticle of activeArticles) {
       activeArticle.classList.remove('active');
     }
 
@@ -41,8 +41,9 @@
   const optArticleSelector = '.post',
     optTitleSelector = '.post-title',
     optTitleListSelector = '.titles';
+    optArticleTagsSelector = '.post-tags .list';
 
-  function generateTitleLinks(){
+  function generateTitleLinks() {
 
     /* remove contents of titleList */
     const titleList = document.querySelector(optTitleListSelector);
@@ -51,8 +52,8 @@
     /* for each article */
     const articles = document.querySelectorAll(optArticleSelector);
     let html = '';
-    for(let article of articles){
-      
+    for (let article of articles) {
+
       /* get the article id */
       const articleId = article.getAttribute('id');
 
@@ -65,20 +66,20 @@
       console.log(linkHTML);
 
       /* insert link into titleList */
-      titleList.innerHTML = titleList.innerHTML + linkHTML;
-      /*option2 
-      titleList.innerHTML.insertAdjacentHTML('beforeend','linkHTML'); */
+      /*titleList.innerHTML = titleList.innerHTML + linkHTML;*/
+
+      titleList.insertAdjacentHTML('beforeend', linkHTML);
 
       /* insert link into html variable */
-      html = html+linkHTML;
+      html = html + linkHTML;
 
     }
 
-    titleList.innerHTML = html;
+    /*titleList.innerHTML = html;*/
 
 
     const links = document.querySelectorAll('.titles a');
-    for(let link of links){
+    for (let link of links) {
       link.addEventListener('click', titleClickHandler);
     }
 
@@ -86,4 +87,38 @@
 
   generateTitleLinks();
 
+  function generateTags(){
+    /* find all articles */
+    const articles = document.querySelectorAll(optArticleSelector);
+    /* START LOOP: for every article: */
+    for (let article of articles) {
+      /* find tags wrapper */
+      const titleList = article.querySelector(optArticleTagsSelector);
+      /* make html variable with empty string */
+      let html = '';
+      /* get tags from data-tags attribute */
+      const articleTags = article.getAttribute('data-tags');
+      console.log (articleTags);
+      /* split tags into array */
+  
+      /* START LOOP: for each tag */
+  
+        /* generate HTML of the link */
+  
+        /* add generated code to html variable */
+  
+      /* END LOOP: for each tag */
+  
+      /* insert HTML of all the links into the tags wrapper */
+  
+    /* END LOOP: for every article: */
+    }
+  }
+  
+  generateTags();
+
 }
+
+
+
+
